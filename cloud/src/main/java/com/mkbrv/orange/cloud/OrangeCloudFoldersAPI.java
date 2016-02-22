77a65327089e622840c1a9110a43dc4f9a682b69
Miklos Csere <miklos.csere@netcentric.biz>
2016-02-22 10:56:53 +0100

    Orange Folders API structure

--- cloud/src/main/java/com/mkbrv/orange/cloud/OrangeCloudFoldersAPI.java
+++ cloud/src/main/java/com/mkbrv/orange/cloud/OrangeCloudFoldersAPI.java
@@ -1,6 +1,7 @@
 package com.mkbrv.orange.cloud;
 
 import com.mkbrv.orange.client.security.OrangeAccessToken;
+import com.mkbrv.orange.cloud.model.OrangeFolder;
 import com.mkbrv.orange.cloud.model.OrangeFreeSpace;
 
 /**
@@ -15,6 +16,41 @@ public interface OrangeCloudFoldersAPI {
      * @return
      */
     OrangeFreeSpace getAvailableSpace(final OrangeAccessToken orangeAccessToken);
+
+
+    /**
+     * @param orangeAccessToken
+     * @return
+     */
+    OrangeFolder getRootFolder(final OrangeAccessToken orangeAccessToken);
+
+    /**
+     * @param orangeAccessToken
+     * @param orangeFolder
+     * @return
+     */
+    OrangeFolder getFolder(final OrangeAccessToken orangeAccessToken, final OrangeFolder orangeFolder);
+
+    /**
+     * @param orangeAccessToken
+     * @param orangeFolder
+     * @return
+     */
+    OrangeFolder createFolder(final OrangeAccessToken orangeAccessToken, final OrangeFolder orangeFolder);
+
+    /**
+     * @param orangeAccessToken
+     * @param orangeFolder
+     * @return
+     */
+    OrangeFolder updateFolder(final OrangeAccessToken orangeAccessToken, final OrangeFolder orangeFolder);
+
+
+    /**
+     * @param orangeAccessToken
+     * @param orangeFolder
+     */
+    void deleteFolder(final OrangeAccessToken orangeAccessToken, final OrangeFolder orangeFolder);
 
 
 }
---
+++ cloud/src/main/java/com/mkbrv/orange/cloud/model/OrangeFolder.java
@@ -0,0 +1,11 @@
+package com.mkbrv.orange.cloud.model;
+
+/**
+ * Created by mikibrv on 21/02/16.
+ */
+public class OrangeFolder {
+
+    String parentFolderId;
+
+
+}
